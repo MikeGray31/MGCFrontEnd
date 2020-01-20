95468727630d1c25195650cf80cb9362af051905
MikeGray31 <michaelkgray31@gmail.com>
2020-01-20 13:14:38 -0500

    added login and create-user-page components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { SideBarComponent } from './side-bar/side-bar.component';
+import { MainAreaComponent } from './main-area/main-area.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { CreateUserPageComponent } from './create-user-page/create-user-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    SideBarComponent
+    SideBarComponent,
+    MainAreaComponent,
+    LoginPageComponent,
+    CreateUserPageComponent
   ],
   imports: [
     BrowserModule,
